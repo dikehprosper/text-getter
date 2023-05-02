b8a253fa003ce43622e649dccd5ff839ec660967
prosper <dikehprosper@gmail.com>
2023-05-02 02:55:13 +0100

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,60 @@ const Home = () => {
                 </div>
                 <div className='body6'>
                     <div className='body7'>Transaction Amount</div>
-                    <div className='body8'>500.00</div>
+                    <h2 className='body8'>500.00</h2>
+                </div>
+                <div className='body9'>
+                    <div className='body11'>
+                        <div className='body10'>Beneficiary Details</div>
+                        <div className='body12'>Ikpeama Malachy Obinna</div>
+                    </div>
+                    <div className='body13'>3457788998</div>
+                </div>
+                <div className='body9'>
+
+                    <div className='body11'>
+                        <div className='body10'>Sender Details</div>
+                        <div className='body12'>Prosper Dike</div>
+
+                    </div>
+                    <div className='body13'>Kuda | 4647383899</div>
+                </div>
+                <div className='body9'>
+
+                    <div className='body11'>
+                        <div className='body10'>Paid On</div>
+                        <div className='body12'>May 01, 2023</div>
+                    </div>
+                    <div className='body13'>8:46 AM</div>
+                </div>
+                <div className='body9'>
+
+                    <div className='body11'>
+                        <div className='body16'>Fees</div>
+                        <div className='body15'>N0.00</div>
+                    </div>
+                    <div className='body13'></div>
+                </div>
+                <div className='body9'>
+                    <h2 className='body11'>
+                        <div className='body16'>Description</div>
+                        <div className='body15'>pay</div>
+                    </h2>
+                    <div className='body13'></div>
+                </div>
+                <div className='body9'>
+                    <div className='body11'>
+                        <div className='body16'>Transaction Reference</div>
+                        <div className='body15'>234567890987654345678987658765</div>
+                    </div>
+                    <div className='body13'></div>
+                </div>
+                <div className='body9'>
+                    <div className='body11'>
+                        <div className='body16'>Payment Type</div>
+                        <div className='body15'>Outward Transfer</div>
+                    </div>
+                    <div className='body13'></div>
                 </div>
             </div>
         </div>
